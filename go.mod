27eebca3faf72197f125c1dc2c402cae393e0d7b
michellekoegelenberg <38162974+michellekoegelenberg@users.noreply.github.com>
2021-08-17 20:48:20 +0200

    Base 58

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/mmcloughlin/avo v0.0.0-20201105074841-5d2f697d268f // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.0 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -5,6 +5,7 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/sha256"
+	"fmt"
 	"log"
 
 	"golang.org/x/crypto/ripemd160"
@@ -19,6 +20,27 @@ const (
 type Wallet struct {
 	PrivateKey ecdsa.PrivateKey
 	PublicKey  []byte
+}
+
+// Meth allows us to generate an address for each of our wallets
+// Need version, checksum and PubKey to create this address
+//Concat these three values together and pass them through a base58 algo to create the address
+func (w Wallet) Address() []byte {
+	pubHash := PublicKeyHash(w.PublicKey)
+
+	versionedHash := append([]byte{version}, pubHash...)
+	checksum := Checksum(versionedHash)
+
+	fullHash := append(versionedHash, checksum...)
+
+	address := Base58Encode(fullHash)
+
+	fmt.Printf("pub key: %x\n", w.PublicKey)
+	fmt.Printf("pub hash: %x\n", pubHash)
+	fmt.Printf("address: %x\n", address)
+
+	return address
+
 }
 
 //Func to generate new Key pair
@@ -55,4 +77,15 @@ func PublicKeyHash(pubKey []byte) []byte {
 		log.Panic(err)
 	}
 
+	PublicRipMdD := hasher.Sum(nil)
+
+	return PublicRipMdD
+
+}
+
+func Checksum(payload []byte) []byte {
+	firstHash := sha256.Sum256(payload)
+	secondHash := sha256.Sum256(firstHash[:])
+
+	return secondHash[:checksumLength]
 }
