cd27a8c375e9e475e06f357229ea9eeb7582f23b
michellekoegelenberg <38162974+michellekoegelenberg@users.noreply.github.com>
2021-08-17 19:43:13 +0200

    PublicKeyHash & Checksum

---
+++ wallet/wallet.go
@@ -0,0 +1,58 @@
+package wallet
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"log"
+
+	"golang.org/x/crypto/ripemd160"
+)
+
+const (
+	checksumLength = 4
+	version        = byte(0x00)
+)
+
+// Wallet contains Pub and Private Key
+type Wallet struct {
+	PrivateKey ecdsa.PrivateKey
+	PublicKey  []byte
+}
+
+//Func to generate new Key pair
+func NewKeyPair() (ecdsa.PrivateKey, []byte) {
+	curve := elliptic.P256()
+
+	private, err := ecdsa.GenerateKey(curve, rand.Reader)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	pub := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
+
+	return *private, pub
+
+}
+
+//Abstraction func which allows us to set them into a wallet type
+
+func MakeWallet() *Wallet {
+	private, public := NewKeyPair()
+	wallet := Wallet{private, public}
+
+	return &wallet
+}
+
+func PublicKeyHash(pubKey []byte) []byte {
+	pubHash := sha256.Sum256(pubKey)
+
+	hasher := ripemd160.New()
+	_, err := hasher.Write(pubHash[:])
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
